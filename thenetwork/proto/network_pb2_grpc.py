a86ae51b7462a28d622e103f35f1aa3ae90b2444
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-06-28 08:52:38 -0600

    Attempt to relative import

--- thenetwork/proto/network_pb2_grpc.py
+++ thenetwork/proto/network_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import network_pb2 as network__pb2
+from . import network_pb2 as network__pb2
 
 
 class TheNetworkStub(object):
